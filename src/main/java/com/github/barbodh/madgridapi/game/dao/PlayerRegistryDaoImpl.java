857a576a7c783a58e1b4780eb227b227dc245818
BarbodH <habibi.k.barbod@gmail.com>
2024-07-28 14:29:02 -0400

    Implemented player registry DAO

--- src/main/java/com/github/barbodh/madgridapi/game/dao/PlayerRegistryDaoImpl.java
+++ src/main/java/com/github/barbodh/madgridapi/game/dao/PlayerRegistryDaoImpl.java
@@ -1,23 +1,41 @@
 package com.github.barbodh.madgridapi.game.dao;
 
+import com.github.barbodh.madgridapi.game.model.PlayerRegistry;
+import com.github.barbodh.madgridapi.util.FirestoreUtil;
+import com.google.cloud.firestore.FieldValue;
+import com.google.cloud.firestore.Firestore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class PlayerRegistryDaoImpl implements PlayerRegistryDao {
+    private final String collectionName = "activePlayers";
+    private final String documentName = "registry";
+    private final Firestore firestore;
+
     @Override
     public void update(String id) {
-
+        var future = firestore.collection(collectionName).document(documentName).update("ids", FieldValue.arrayUnion(id));
+        FirestoreUtil.awaitCompletion(future);
     }
 
     @Override
     public boolean exists(String id) {
+        var future = firestore.collection(collectionName).document(documentName).get();
+        var documentSnapshot = FirestoreUtil.awaitCompletion(future);
+
+        if (documentSnapshot.exists()) {
+            var registry = documentSnapshot.toObject(PlayerRegistry.class);
+            return registry != null && registry.getIds().contains(id);
+        }
+
         return false;
     }
 
     @Override
     public void delete(String id) {
-
+        var future = firestore.collection(collectionName).document(documentName).update("ids", FieldValue.arrayRemove(id));
+        FirestoreUtil.awaitCompletion(future);
     }
 }
